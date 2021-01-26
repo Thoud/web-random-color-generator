8f141526de1447926b6d21c2bef8beec4649a7fa
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-01-26 16:23:01 +0100

    Add a CSS-in-JS library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import './Styles.css';
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+import { useState, useEffect } from 'react';
 
 export default function App() {
   return <div></div>;
