7ab299cd2f08d838bdaed6b4d294faf4cd484766
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-01-27 19:40:07 +0100

    Finalise styling

--- src/js/App.js
+++ src/js/App.js
@@ -102,6 +102,7 @@ export default function App() {
         setWidth={setWidth}
         height={height}
         setHeight={setHeight}
+        textColor={textColor}
       />
       <RandomColorBox
         backgroundColor={backgroundColor}
--- src/js/ParamDisplay.js
+++ src/js/ParamDisplay.js
@@ -1,6 +1,125 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+
 export default function ParamDisplay(props) {
+  // CSS-in-JS Styling
+  const rangeStyling = css`
+    input[type='range'] {
+      width: 100%;
+      margin: 3.95px 0;
+      background-color: transparent;
+      -webkit-appearance: none;
+    }
+
+    input[type='range']:focus {
+      outline: none;
+    }
+
+    input[type='range']::-webkit-slider-runnable-track {
+      background: hsl(${props.hue}, ${props.saturation}%, ${props.lightness}%);
+      transition: background 0.5s ease-in-out;
+      border: 0;
+      border-radius: 25px;
+      width: 100%;
+      height: 8.1px;
+      cursor: pointer;
+    }
+
+    input[type='range']::-webkit-slider-thumb {
+      margin-top: -3.95px;
+      width: 16px;
+      height: 16px;
+      background: #ffffff;
+      border: 1px solid #000000;
+      border-radius: 50px;
+      cursor: pointer;
+      -webkit-appearance: none;
+    }
+
+    input[type='range']:focus::-webkit-slider-runnable-track {
+      background: hsl(${props.hue}, ${props.saturation}%, ${props.lightness}%);
+      transition: background 0.5s ease-in-out;
+    }
+
+    input[type='range']::-moz-range-track {
+      background: hsl(${props.hue}, ${props.saturation}%, ${props.lightness}%);
+      transition: background 0.5s ease-in-out;
+      border: 0;
+      border-radius: 25px;
+      width: 100%;
+      height: 8.1px;
+      cursor: pointer;
+    }
+
+    input[type='range']::-moz-range-thumb {
+      width: 16px;
+      height: 16px;
+      background: #ffffff;
+      border: 1px solid #000000;
+      border-radius: 50px;
+      cursor: pointer;
+    }
+
+    input[type='range']::-ms-track {
+      background: transparent;
+      border-color: transparent;
+      border-width: 4.95px 0;
+      color: transparent;
+      width: 100%;
+      height: 8.1px;
+      cursor: pointer;
+    }
+
+    input[type='range']::-ms-fill-lower {
+      background: hsl(${props.hue}, ${props.saturation}%, ${props.lightness}%);
+      transition: background 0.5s ease-in-out;
+      border: 0;
+      border-radius: 50px;
+    }
+
+    input[type='range']::-ms-fill-upper {
+      background: hsl(${props.hue}, ${props.saturation}%, ${props.lightness}%);
+      transition: background 0.5s ease-in-out;
+      border: 0;
+      border-radius: 50px;
+    }
+
+    input[type='range']::-ms-thumb {
+      width: 16px;
+      height: 16px;
+      background: #ffffff;
+      border: 1px solid #000000;
+      border-radius: 50px;
+      cursor: pointer;
+      margin-top: 0px;
+    }
+
+    input[type='range']:focus::-ms-fill-lower {
+      background: hsl(${props.hue}, ${props.saturation}%, ${props.lightness}%);
+      transition: background 0.5s ease-in-out;
+    }
+
+    input[type='range']:focus::-ms-fill-upper {
+      background: hsl(${props.hue}, ${props.saturation}%, ${props.lightness}%);
+      transition: background 0.5s ease-in-out;
+    }
+  `;
+
+  const buttonStyling = css`
+    background: hsl(${props.hue}, ${props.saturation}%, ${props.lightness}%);
+    color: ${props.textColor};
+    transition: background 0.5s ease-in-out, color 0.5s ease-in-out;
+    border: none;
+    text-align: center;
+    padding: 1rem;
+    font-weight: 600;
+    border-radius: 1rem;
+    font-size: 16px;
+  `;
+
   return (
-    <section id="sectionOne">
+    <section id="sectionOne" css={rangeStyling}>
       <h1>Random Color Generator</h1>
 
       <div className="group">
@@ -101,6 +220,7 @@ export default function ParamDisplay(props) {
           props.setSaturation(Math.floor(Math.random() * 100));
           props.setLightness(Math.floor(Math.random() * 100));
         }}
+        css={buttonStyling}
       >
         Random Color
       </button>
